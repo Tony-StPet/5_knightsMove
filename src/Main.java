ea8fc6fa7ae2e8584d7e1ec859e28f662d5b1339
Tony-StPet <135145428+Tony-StPet@users.noreply.github.com>
2024-09-10 22:54:28 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.print("Введите символ: ");
+        char symbol = scanner.nextLine().charAt(0);
+        int symbolNumber = convertSymbolToNumber(symbol);
+        char symbolBack = convertNumberToSymbol(symbolNumber);
+
+        System.out.print("Введите число: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Варианты хода рыцаря:");
+        if (symbolNumber + 2 <= 8 && number + 1 <= 8) {
+            char knightMoveSymbol = convertNumberToSymbol(symbolNumber + 2);
+        System.out.println("- " + knightMoveSymbol + ", " + (number + 1));}
+        if (symbolNumber + 2 <= 8 && number - 1 >= 1) {
+            char knightMoveSymbol = convertNumberToSymbol(symbolNumber + 2);
+        System.out.println("- " + knightMoveSymbol + ", " + (number - 1));}
+
+        if (symbolNumber - 2 >= 1 && number + 1 <= 8) {
+            char knightMoveSymbol = convertNumberToSymbol(symbolNumber - 2);
+        System.out.println("- " + knightMoveSymbol + ", " + (number + 1));}
+        if (symbolNumber - 2 >= 1 && number - 1 >= 1) {
+            char knightMoveSymbol = convertNumberToSymbol(symbolNumber - 2);
+        System.out.println("- " + knightMoveSymbol + ", " + (number - 1));}
+
+        if (symbolNumber + 1 <= 8 && number + 2 <= 8) {
+            char knightMoveSymbol = convertNumberToSymbol(symbolNumber + 1);
+        System.out.println("- " + knightMoveSymbol + ", " + (number + 2));}
+
+        if (symbolNumber - 1 >= 1 && number + 2 <= 8) {
+            char knightMoveSymbol = convertNumberToSymbol(symbolNumber - 1);
+        System.out.println("- " + knightMoveSymbol + ", " + (number + 2));}
+
+        if (symbolNumber + 1 <= 8 && number - 2 >= 1) {
+            char knightMoveSymbol = convertNumberToSymbol(symbolNumber + 1);
+        System.out.println("- " + knightMoveSymbol + ", " + (number - 2));}
+
+        if (symbolNumber - 1 >= 1 && number - 2 >= 1) {
+            char knightMoveSymbol = convertNumberToSymbol(symbolNumber - 1);
+        System.out.println("- " + knightMoveSymbol + ", " + (number - 2));}
+    }
+
+    public static int convertSymbolToNumber(char symbol) {
+        // Переводим символ в нижний регистр, чтобы учесть 'A'-'H'
+        symbol = Character.toLowerCase(symbol);
+
+        // Вычисляем соответствующее число, отнимая код символа 'a'
+        return symbol - 'a' + 1;
+    }
+
+    public static char convertNumberToSymbol(int number) {
+        // Вычисляем соответствующий символ, прибавляя код символа 'a' и приводя к char
+        return (char)(number + 'a' - 1);
     }
 }
